8c7de7a9f14f1da6995e414184ee768d546a3354
sixfwa <sixfwali@gmail.com>
2024-05-24 18:49:59 +0100

    csr and ssr

---
+++ src/app/csr/page.tsx
@@ -0,0 +1,34 @@
+"use client"
+import Container from "@/app/components/Container"
+import Header from "@/app/components/Header"
+import { useEffect, useState } from "react"
+import { Post } from "@/app/types"
+
+export default function CSR() {
+  const [posts, setPosts] = useState<Post[]>([])
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+      const data: Post[] = await response.json()
+      setPosts(data)
+    }
+
+    fetchPosts()
+  }, [])
+  return (
+    <Container>
+      <Header>Client-Side Rendering</Header>
+      <ul className="flex flex-col gap-2">
+        {posts.map((post) => (
+          <li
+            key={post.id}
+            className="p-3 border rounded border-neutral-700 flex flex-col gap-2"
+          >
+            <h2 className="text-xl font-bold tracking-tight">{post.title}</h2>
+            <p className="text-neutral-200 tracking-wide">{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
---
+++ src/app/ssr/page.tsx
@@ -0,0 +1,31 @@
+import Container from "@/app/components/Container"
+import Header from "@/app/components/Header"
+import { Post } from "../types"
+
+async function getPosts() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-cache",
+  })
+  const data: Post[] = await res.json()
+  return data
+}
+
+export default async function SSR() {
+  const posts = await getPosts()
+  return (
+    <Container>
+      <Header>Server-Side Rendering</Header>
+      <ul className="flex flex-col gap-2">
+        {posts.map((post) => (
+          <li
+            key={post.id}
+            className="p-3 border rounded border-neutral-700 flex flex-col gap-2"
+          >
+            <h2 className="text-xl font-bold tracking-tight">{post.title}</h2>
+            <p className="text-neutral-200 tracking-wide">{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
